fa2a88568d4c0b8453f4b99a561c0c5eaa4d786b
Nate Woods <big.nate.w@gmail.com>
2016-08-24 23:22:57 -0600

    Wrapping in static files better

--- web/web.go
+++ web/web.go
@@ -3,14 +3,35 @@ package web
 //go:generate appcfg.py update .
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/user"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/", index)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
+var (
+	indexTPL = template.Must(template.ParseFiles("index.html"))
+	static   = http.FileServer(http.Dir("static"))
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		static.ServeHTTP(w, r)
+		return
+	}
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	indexTPL.Execute(w, map[string]interface{}{
+		"user": u.String(),
+	})
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	// TODO: process post
 }
